7939ae476272f7893724328ced970005b121cb1a
joy164 <joelvillar35@gmail.com>
2021-07-12 15:17:16 -0500

    codigo de commit anterior incompleto

--- pong.py
+++ pong.py
@@ -25,11 +25,17 @@ while gameOver  == False:
     
     close_verify()
     screen.fill(negro)
+    #crea los elementos del juego 
     #contorno(screen, blanco, 20, 30, 760, 440, 5)
     cancha(screen, blanco,400, 30, 440, 50, 1)
     texto(screen, blanco, 600, 60, 55, str(contadorJ2).zfill(2), consolas)
     texto(screen, blanco, 200, 60, 55, str(contadorJ1).zfill(2), consolas)
-   
+    #evaluacion de los puntajes de los jugadores
+    if(contadorJ1 ==  5):
+        gameOver = True  
+    elif(contadorJ2 == 5):
+        gameOver = True                    
+    #lectura del teclado para el jugador 1
     tecla = pygame.key.get_pressed()
     if(tecla[K_s]):          
         if (pos_y1 < 350 and pos_y1 >= 90):
@@ -39,6 +45,7 @@ while gameOver  == False:
             pos_y1 -= speed
     else:
         pass
+    #lectura del teclado para el jugador 2
     tecla = pygame.key.get_pressed()
     if(tecla[K_DOWN]):          
         if (pos_y2 < 350 and pos_y2 >= 90):
@@ -48,20 +55,21 @@ while gameOver  == False:
             pos_y2 -= speed
     else:
         pass
+    
+    #limite de la pelota  en la cancha
     if (pelotaX <= 32 or pelotaX >= 770):
         speedPelotaX *= -1
     if (pelotaY <= 95 or pelotaY >= 445):
         speedPelotaY *= -1
-
-    if(contadorJ1 ==  5):
-        gameOver = True  
-    elif(contadorJ2 == 5):
-        gameOver = True        
-        
+    #mueve la pelota
+    pelotaX += speedPelotaX
+    pelotaY += speedPelotaY    
+    #evalua si la pelota colisiona con la raqueta del jugador 1
     if(pelotaY - 5 >= pos_y1 and pelotaY  -5 <= pos_y1 + 100):
         if(pelotaX <= 90 and pelotaX >= 70):
-            speedPelotaX *= -1
-
+            speedPelotaX *= -1    
+    
+    #evalua si la pelota colisiona con la porteria del jugador 1
     if(pelotaX < 65):
         contadorJ2 += 1
         if(contadorJ2 < 5):
@@ -71,17 +79,36 @@ while gameOver  == False:
             refresh_screen(clock, 60)
             pygame.time.delay(1000)
 
-
-    pelotaX += speedPelotaX
-    pelotaY += speedPelotaY
+    #evalua si la pelota colisiona con la raqueta del jugador 2
+    if(pelotaY - 5 >= pos_y2 and pelotaY  -5 <= pos_y2 + 100):
+        if(pelotaX >= 710 and pelotaX <= 730):
+            speedPelotaX *= -1    
+            
+            
+    
+    #evalua si la pelota colisiona con la porteria del jugador 2
+    if(pelotaX > 765):
+        contadorJ1 += 1
+        if(contadorJ1 < 5):
+            pelotaX = 400 
+            screen.fill(negro)
+            texto(screen, blanco, 400, 200, 30, "Jugador 1 a anotado!!", consolas)  
+            refresh_screen(clock, 60)
+            pygame.time.delay(1000)            
+    
+    #zona de dibujo de los elementos del juego 
     pelota(screen, blanco, pelotaX, pelotaY, 10)
     raqueta(screen, blanco, 70, pos_y1, 100, 20)
     raqueta(screen, blanco, 710, pos_y2, 100, 20)
-    
+    #actualiza la pantalla
     refresh_screen(clock, 60)
-
+#pinta la pantalla y los elementos del juego quedan ocultos
 screen.fill(negro)
-texto(screen, blanco, 400, 200, 30, "FIN DE JUEGO", consolas)  
+texto(screen, blanco, 400, 200, 30, "FIN DE JUEGO", consolas) 
+if( contadorJ1 == 5):
+    texto(screen, blanco, 400, 250, 30, "jugador 1 gana", consolas) 
+elif(contadorJ2 == 5):
+    texto(screen, blanco, 400, 250, 30, "jugador 2 gana", consolas) 
 refresh_screen(clock, 60)
 pygame.time.delay(1000)
 
